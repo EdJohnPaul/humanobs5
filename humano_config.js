dca3f302697806da3aade38852dbb9adb4bcf41f
EdJohnPaul <edjohnpaulgallardo28@gmail.com>
2024-05-27 10:16:03 +0800

    added changes

--- humano_config.js
+++ humano_config.js
@@ -1,7 +1,7 @@
 var Config = {
     url: $.parseJSON($.ajax({
         type: "GET",
-        url: "https://www.humanobs5.com/humano_config.json",
+        url: "http://edjohnpaul.github.io/humano_config.json",
         dataType: "json",
         global: false,
         async: false,
